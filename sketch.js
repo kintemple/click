8340b65c7fbf9d8658cb897a91817b3bec203934
kintemple <kitemple@iu.edu>
2026-04-01 18:40:22 -0400

    test

--- sketch.js
+++ sketch.js
@@ -4,9 +4,12 @@ function setup() {
 
 function draw() {
   background(220);
+  noStroke();
   fill('yellow'); 
   ellipse(100, 100, 200, 200); 
 
   fill('white');
-  ellipse(200, 200, 20, 20);
+  ellipse(200, 200, 40, 40);
+  ellipse(210, 210, 40, 40);
+  ellipse(215, 215, 40, 40);
 }
